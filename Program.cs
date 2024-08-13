d5f81679dc50dddc00f109804fbc7c54d967a921
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2024-08-13 16:44:40 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DelegateExampleLesson1
+{
+    //1. first create a delegate
+    public delegate double Operation(double x, double y);
+
+    public class Program
+    {
+        //2. create a static method to be referenced to delegate in main method
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static void Main(string[] args)
+        {
+            //3. create a variable for the reference type delegate which is an implemetation of Abstraction
+            Operation addOperation = Add;
+            double num1 = 10.5;
+            double num2 = 12.3;
+
+            //pass values to the static method Add
+            double result = addOperation(num1, num2);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
